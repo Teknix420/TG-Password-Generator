e0f3a1dc7039fa1d31971031b959ec36e5b2ac8f
Teknix <pandapaloo@gmail.com>
2020-08-03 19:40:50 -0600

    Refined Javascript and repositioned some code for better efficiency and added some notes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Declaring character criteria variables //
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var numerical = '0123456789';
+var specialChar = '!@#$%&*?/+-_~';
+var passGen = '';
+
+
 // Write password to the #password input
 function writePassword() {
-
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -14,6 +22,10 @@ function writePassword() {
 
 function generatePassword() {
 
+  // Clearing password and passGen Strings
+  password = '';
+  passGen = '';
+
   // Prompting and establishing variables //
   var passLength = prompt("Enter a password length from 8 to 128");
 
@@ -23,61 +35,57 @@ function generatePassword() {
   // Validating the selected password length is between 8 and 12 characters
   if (passLength >= 8 && passLength <= 128) {
 
+    // Prompting for password criteria //
     var passSpecial = confirm("Do you want to include special characters?");
     var passUpper = confirm("Do you want to include Uppercase Letters?");
     var passLower = confirm("Do you want to include Lowercase letters?");
     var passNumerical = confirm("Do you want to add numbers to your password?");
 
+    // 
     if (passSpecial === false && passUpper === false && passLower === false && passNumerical === false) {
+
       alert("You need to pick at least one character type for your password!!")
       generatePassword();
+
     }
+
     else {
-      var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-      var numerical = '0123456789';
-      var specialChar = '!@#$%&*?/+-_~';
-      var passGen = '';
 
-      // Clearing password string //
-      password = '';
-
-
-      // Validating the selected password length is between 8 and 12 characters
       // Combining chosen password criteria into one big string //
       if (passSpecial === true) {
         passGen = passGen.concat(specialChar);
       }
-
+      
       if (passUpper === true) {
         passGen = passGen.concat(upperCase);
       }
-
+      
       if (passLower === true) {
         passGen = passGen.concat(lowerCase);
       }
-
+      
       if (passNumerical === true) {
         passGen = passGen.concat(numerical);
       }
-
+      
       // Taking newly created string and selecting random characters from it //
       for (i = 0; i < passLength; i++) {
         password += passGen.charAt(Math.floor(Math.random() * passGen.length));
       }
     }
-
+    
   }
-
+  
   else {
+
     alert("You didn't select a number between 8 and 128! Try again!");
     generatePassword();
+
   }
-
-  // Sending the chosen password back to the index file and clearing the temp passGen string //
+  
+  // Sending the chosen password back to the index file //
   return password;
-  passGen = '';
-
+  
 }
 
 // Add event listener to generate button
